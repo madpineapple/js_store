c32d7d0c5ff158290f981dcb8042806d8bac0875
Charles Daoust <daoustcharles@yahoo.com>
2019-10-21 03:38:07 -0400

    working on receit function

--- routes/app.js
+++ routes/app.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const {ensureAuthenticated} = require('../config/auth');
 const Cart = require("../models/cart");
 const keyPublishable = '';
-const keySecret = process.env.SECRET_KEY;
-const stripe = require('stripe')(process.env.SECRET_KEY);
+const keySecret = '';
+const stripe = require('stripe')('');
+const nodemailer = require('nodemailer');
 
 //User model
 const db = require('../models/User');
@@ -183,13 +184,55 @@ router.get('/checkout2',(req,res)=>{
     let customers = [name,  address, city, country, zip, email]
     let sql = "INSERT INTO customers (name,  address, city, country, zip, email) VALUES (?, ?, ?,?,?,?);"
  db.query(sql, customers, (err, result)=>{
-   if (err){
-     throw err;
-   }else{
+     if (err){
+       throw err;
+     }else{
      console.log('succesfully insetred customers');
      console.log(result.insertId);
      let custID = result.insertId;
      insertOrders(cart,totalPrice, custID);
+     const output =`
+       <h3>Your order is on route!</h3>
+       <p>Your delivery address</p>
+       <p>${customers}</p>
+       <p>Total</p>
+       <p>${totalPrice}</p>
+     `;
+     async function main() {
+         // Generate test SMTP service account from ethereal.email
+         // Only needed if you don't have a real mail account for testing
+         let testAccount = await nodemailer.createTestAccount();
+
+         // create reusable transporter object using the default SMTP transport
+         let transporter = nodemailer.createTransport({
+             host: 'smtp.ethereal.email',
+             port: 587,
+             secure: false, // true for 465, false for other ports
+             auth: {
+                 user: testAccount.user, // generated ethereal user
+                 pass: testAccount.pass // generated ethereal password
+             }
+         });
+
+         // send mail with defined transport object
+         let info = await transporter.sendMail({
+             from: '"Fred Foo 👻" <foo@example.com>', // sender address
+             to: 'bar@example.com, baz@example.com', // list of receivers
+             subject: 'Hello ✔', // Subject line
+             text: 'Hello world?', // plain text body
+             html: output  // html body
+
+         });
+
+         console.log('Message sent: %s', info.messageId);
+         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+         // Preview only available when sending through an Ethereal account
+         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+     }
+
+     main().catch(console.error);
    }
  });
 
